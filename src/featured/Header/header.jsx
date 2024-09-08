465e7733d926706e4b9833acfa1489cac91494d4
Ulvi <ulvijalil7@gmail.com>
2024-09-08 15:10:19 +0400

    feat: Added i18 to header and created aze eng ru buttons

--- src/featured/Header/header.jsx
+++ src/featured/Header/header.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import { FaDesktop } from "react-icons/fa";
